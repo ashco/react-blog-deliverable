c861b0e3a1544c3ccc4e27251c65d6482c662a2d
ashco <ashchristie@gmail.com>
2018-01-17 16:57:57 -0800

    lesson finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Author from './Author.js';
 import './App.css';
 
 class Post extends Component {
+  // want to attach a value to component? needs to be done through constructor function
+  constructor(props){
+    super(props)
+    // let this.x = 25;
+    this.state = {
+      moodPoints: 1
+    }
+  }
+   //makes sense to write on one line
+  increaseMood(e){
+    // if(this.state.moodPoints <= 9){
+    //   this.setState({moodPoints: this.state.moodPoints + 1})
+    //  }else{
+    //   this.setState({moodPoints:  1})
+    // }
+    
+    //Ternery Operator is way better than if / else
+    this.state.moodPoints <= 9?this.setState({moodPoints: this.state.moodPoints + 1}):this.setState({moodPoints:  1})
+
+  }
+
+
   render() {
     const myPosts = this.props.posts.map(p => {
       return <div key={p.title}>
@@ -27,6 +49,9 @@ class Post extends Component {
             All the latest and greatest things from 65 million years ago.
           </p>
         </header>
+        <h3>On a scale of 1 to 10, this is how I feel...</h3>
+        <p>Current Mood: {this.state.moodPoints}</p>
+        <button onClick={(e)=> this.increaseMood(e)}>Jolly Butt's</button>
         {myPosts}
         <em>Note, at this stage, we are only rendering ONE post with ONE comment!</em>
       </div>
